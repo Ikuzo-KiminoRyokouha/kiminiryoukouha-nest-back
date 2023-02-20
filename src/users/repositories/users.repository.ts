5a6fb67765f9c9217873704f460b0fc90e60471b
SHIN_MINGYU <smg20004@naver.com>
2023-02-20 15:32:38 +0900

    🐛 fix : user follow logic change

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -95,10 +95,10 @@ export class UserRespository {
       });
 
       // 팔로우를 누른 사람에게는 팔로워를 추가해줍니다.
-      sourceUser.followees = [...sourceUser.followers, targetUser];
+      sourceUser.followees = [...sourceUser.followees, targetUser];
 
       // 팔로우를 눌려진 사람에게는 팔로이를 추가해줍니다.
-      targetUser.followers = [...targetUser.followees, sourceUser];
+      targetUser.followers = [...targetUser.followers, sourceUser];
 
       this.userRepository.save(sourceUser);
       this.userRepository.save(targetUser);
