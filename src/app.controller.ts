c5c4d45d08817d3e2e668a7dd829f0cf4fbb18e1
jinBaeng <jinbaeng88@gmail.com>
2023-05-23 11:28:50 +0900

    🔨 chore : bankging

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import axios from 'axios';
 import { error } from 'console';
 import { PythonShell } from 'python-shell';
 import { throwError } from 'rxjs';
+import { planRepository } from './travels/repositories/plan.repository';
 
 @Controller()
 export class AppController {
+  // constructor(private planRepository: planRepository) {}
+
   @Get()
   sayHello() {
     return 'hello';
   }
+
+  // @Get('test')
+  // async test(@Body() testInput) {
+  //   console.log(testInput);
+  //   const plan = await this.planRepository.showPlan(testInput.planId);
+  //   const endDay = plan.end.toISOString().slice(0, 10).split('-').join('');
+  //   const startDay = plan.start.toISOString().slice(0, 10).split('-').join('');
+  //   console.log(startDay, endDay);
+  //   const nowDate = new Date().toISOString().slice(0, 10).split('-').join('');
+  //   console.log(startDay > nowDate);
+  //   console.log(endDay >= nowDate);
+
+  //   console.log(nowDate);
+  // }
 
   // @Cron('0 1 23 * * *')
   @Cron('0 38 * * * *')
